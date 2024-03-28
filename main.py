6ac55e1c8e1523d357c9f4f55776cb9dc7c0208c
ArtemLobk <lobk.artem@gmail.com>
2024-03-28 15:44:26 +0200

    telegram_bot

--- main.py
+++ main.py
@@ -31,9 +31,11 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message.text.lower()
 
     if 'привіт' in message:
-         reply_text = f'привіт {update.effective_user.first_name}!'
+        reply_text = f'привіт {update.effective_user.first_name} {update.effective_user.last_name}'
+    elif 'допобачення' in message:
+        reply_text = f'допобачення {update.effective_user.first_name} {update.effective_user.last_name}'
     else:
-         reply_text = 'Я вас не розумію.'
+        reply_text = f'я вас не розумію!'
 
     await update.message.reply_text(reply_text)
 
@@ -41,7 +43,7 @@ app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("question", question))
-app.add_handler(CommandHandler("start", start ))
+app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 app.run_polling()
